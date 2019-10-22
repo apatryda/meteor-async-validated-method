256b2323298780b6a1d9205ae2049f8981a5c926
Artur Nerkowski <a.apatryda@gmail.com>
2019-10-23 01:35:33 +0200

    Update typings

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,7 @@
 /// <reference path="../typings/index.d.ts" />
 import { ValidatedMethod as RawValidatedMethod, ValidatedMethodApplyOptions, ValidatedMethodArgs, ValidatedMethodCallback, ValidatedMethodMixin, ValidatedMethodOptions, ValidatedMethodResult } from 'meteor/mdg:validated-method';
 export declare type ValidatedMethodCallAsyncFn<Args extends ValidatedMethodArgs = undefined, Result extends ValidatedMethodResult = void> = (this: RawValidatedMethod<Args, Result>, args?: Args) => Promise<Result>;
+export declare type ValidatedMethodCallAsyncFnWrapper<Args extends ValidatedMethodArgs = undefined, Result extends ValidatedMethodResult = void> = (self: RawValidatedMethod<Args, Result>, args?: Args) => Promise<Result>;
 export declare class ValidatedMethod<Args extends ValidatedMethodArgs = undefined, Result extends ValidatedMethodResult = void> extends RawValidatedMethod<Args, Result> {
     callAsync: ValidatedMethodCallAsyncFn<Args, Result>;
 }
